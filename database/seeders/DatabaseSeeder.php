61cee37aaa511cf64855892138b885055b93e7fe
Carlos-Xavier <cexb@ic.ufal.br>
2023-10-25 19:51:49 -0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
-    {
-        \App\Models\User::factory(30)->create();
+    public function run(): void {
+        $this->call([
+            UsuarioTipoSeeder::class,
+            TutoriaStatusSeeder::class,
+            LoginSeeder::class,
+            EspecialidadeSeeder::class,
+            UserSeeder::class,
+            SubtopicoSeeder::class,
+        ]);
     }
 }
